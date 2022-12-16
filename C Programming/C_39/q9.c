878272437c489059ae849de19d8e439bab2472e2
Tusher Mondal <luciefer9062hurley.com>
2022-12-16 18:16:19 +0530

    q9 finalized

--- C Programming/C_39/q9.c
+++ C Programming/C_39/q9.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-char convertUpper(char c);
+void copy(FILE *t, FILE *d);
 int main()
 {
     FILE *file1, *file2;
-    char temp;
     file1 = fopen("/home/tusher/Desktop/Codes/MCA-Codes/C Programming/C_39/myfiles/file1.txt", "r");
     file2 = fopen("/home/tusher/Desktop/Codes/MCA-Codes/C Programming/C_39/myfiles/file2.txt", "a");
+    copy(file1, file2);
+    fclose(file1);
+    fclose(file2);
+    return 0;
+}
+void copy(FILE *t, FILE *d)
+{
+    char temp;
     while(1)
     {
-        temp = getc(file1);
+        temp = getc(t);
         if(temp != EOF)
         {
-            putc(temp, file2);
+            putc(temp, d);
         }
         else
         {
             break;
         }  
     }
-    fclose(file1);
-    fclose(file2);
-    return 0;
-}
-char convertUpper(char c)
-{
-    return (c-32);
 }
