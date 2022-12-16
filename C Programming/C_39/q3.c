86c91e979eb8d155b60c14b7ce0b6dc61d617e0e
Tusher Mondal <luciefer9062hurley.com>
2022-12-16 12:14:50 +0530

    q3 added

--- C Programming/C_39/q3.c
+++ C Programming/C_39/q3.c
@@ -5,6 +5,7 @@ typedef struct
     long long acc_num, curr_bal;
     char fname[maxn], lname[maxn];
 } Janata;
+void function1(Janata c[], int n);
 void takeinp(Janata *c);
 int main()
 {
@@ -14,16 +15,29 @@ int main()
     Janata c[n];
     for(int i=0; i<n; i++)
     {
-
+        printf("Enter the data of customer %d\n", i+1);
+        takeinp(&c[i]);
     }
+    printf("Displaying all name of the customers with account balance less than 1000/- .\n");
+    function1(c, n);
     return 0;
+}
+void function1(Janata c[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        if(c[i].curr_bal < 1000)
+        {
+            printf("%s %s\t", c[i].fname, c[i].lname);
+        }
+    }
 }
 void takeinp(Janata *c)
 {
     printf("Account Holder : ");
     scanf("%s %s", c->fname, c->lname);
     printf("Account Number : ");
-    scanf("%ld", c->acc_num);
+    scanf("%lld", &c->acc_num);
     printf("Account Balance : ");
-    scanf("%ld", c->curr_bal);
+    scanf("%lld", &c->curr_bal);
 }
