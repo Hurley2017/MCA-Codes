8274370bcc09581458f4888e7314e6b8d5f3c708
Tusher Mondal <luciefer9062hurley.com>
2022-12-16 18:12:21 +0530

    q9 done

--- C Programming/C_39/q9.c
+++ C Programming/C_39/q9.c
@@ -6,20 +6,13 @@ int main()
     FILE *file1, *file2;
     char temp;
     file1 = fopen("/home/tusher/Desktop/Codes/MCA-Codes/C Programming/C_39/myfiles/file1.txt", "r");
-    file2 = fopen("/home/tusher/Desktop/Codes/MCA-Codes/C Programming/C_39/myfiles/file2.txt", "w");
+    file2 = fopen("/home/tusher/Desktop/Codes/MCA-Codes/C Programming/C_39/myfiles/file2.txt", "a");
     while(1)
     {
         temp = getc(file1);
         if(temp != EOF)
         {
-            if(temp >=97 && temp <=122)
-            {
-                putc(convertUpper(temp), file2);
-            }
-            else
-            {
-                putc(temp, file2);
-            }
+            putc(temp, file2);
         }
         else
         {
