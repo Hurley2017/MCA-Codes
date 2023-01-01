c7175f02b4ad4bd0c81e500bc4be2b8dd598f3ea
Hurley2017 <luciefer9062hurley.com>
2023-01-01 23:40:11 +0530

    Cpp Question 1 added

---
+++ C++/q1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+#define pI 3.1415
+using namespace std;
+class Circle
+{
+    private:
+        float pri_rad;
+    protected:
+        float pro_rad;
+    public:
+        float pub_rad;
+    void getpriVal()
+    {
+        cout << "Getting input for private variable.\n";
+        cin >> pri_rad;
+    }
+    void getproVal()
+    {
+        cout << "Getting input for protected variable.\n";
+        cin >> pro_rad;
+    }
+    void getpubVal()
+    {
+        cout << "Getting input for public variable.\n";
+        cin >> pub_rad;
+    }
+    double calcpriArea()
+    {
+        return pI*pow(pri_rad, 2);
+    }
+    double calcproArea()
+    {
+        return pI*pow(pro_rad, 2);
+    }
+    double calcpubArea()
+    {
+        return pI*pow(pub_rad, 2);
+    }
+    double general(float fromMain)
+    {
+        return pI*pow(fromMain, 2);
+    }
+};
+int main()
+{
+    Circle ob;
+    return 0;
+}
